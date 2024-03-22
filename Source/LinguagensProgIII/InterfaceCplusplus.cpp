8be6e44d38c95e82801072e45bd98a8bef3110ad
SabrinaQuem <sabrimschiochet@gmail.com>
2024-03-22 15:28:15 +0000

    Projeto Apenas com Blueprints (antes da aula de C++)

---
+++ Source/LinguagensProgIII/InterfaceCplusplus.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InterfaceCplusplus.h"
+
+// Add default functionality here for any IInterfaceCplusplus functions that are not pure virtual.
